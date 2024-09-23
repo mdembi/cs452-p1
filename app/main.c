c2c2db6c41172ff7c0fae3da8dfd2dc4a7e3da67
BoiseState Student <student@u.boisestate.edu>
2024-09-23 16:55:57 -0600

    completes task 3

--- app/main.c
+++ app/main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include "../src/lab.h"
 
-int main(void)
+int main(int argc, char **argv) 
 {
-  printf("hello world\n");
-  return 0;
+    parse_args(argc, argv);
+    return 0;
 }
--- src/lab.c
+++ src/lab.c
@@ -1,1 +1,21 @@
-/**Update this file with the starter code**/
+#include <stdio.h>
+#include <unistd.h>
+#include "lab.h"
+
+
+void parse_args(int argc, char **argv) {
+    int opt;
+    
+    // Parse command line
+    while ((opt = getopt(argc, argv, "v")) != -1) {
+        switch (opt) {
+            case 'v':
+                // Print version and exit
+                printf("Version: %d.%d\n", lab_VERSION_MAJOR, lab_VERSION_MINOR);
+                exit(0);
+            default:
+                exit(1);
+        }
+    }
+}
+
